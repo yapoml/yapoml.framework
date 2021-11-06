bd33d9c74f5f912a8d30963219027d78f2cb175a
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2021-11-06 15:09:10 +0300

    Add skeleton for selenium

---
+++ sample/Yapoml.Selenium.Sample/UnitTest1.cs
@@ -0,0 +1,7 @@
+namespace BBB
+{
+    class Tests
+    {
+
+    }
+}
---
+++ src/Yapoml.Selenium/SeleniumGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Microsoft.CodeAnalysis;
+
+namespace Yapoml.Selenium
+{
+    [Generator]
+    public class SeleniumGeneratorGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            foreach (AdditionalText file in context.AdditionalFiles)
+            {
+                if (file.Path.EndsWith(".po.yaml", StringComparison.OrdinalIgnoreCase))
+                {
+                    context.AddSource("E_" + Path.GetFileNameWithoutExtension(file.Path), $@"
+namespace AAA
+{{
+  class {Path.GetFileNameWithoutExtension(file.Path).Replace(" ", "").Replace("-", "").Replace(".", "_")}
+  {{
+
+  }}
+}}
+");
+                }
+            }
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            // No initialization required for this one
+        }
+    }
+}
+

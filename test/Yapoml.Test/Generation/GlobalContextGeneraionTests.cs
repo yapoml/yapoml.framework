4dab9062317c462cd2ce0d08ecafa5b48e805ed3
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2021-11-18 15:00:58 +0300

    Added asserts

--- test/Yapoml.Test/Generation/GlobalContextGeneraionTests.cs
+++ test/Yapoml.Test/Generation/GlobalContextGeneraionTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 using Yapoml.Generation;
 
 namespace Yapoml.Test.Generation
@@ -12,6 +13,17 @@ namespace Yapoml.Test.Generation
 
             gc.AddFile("/some/path/any/other/file1.po.yaml");
             gc.AddFile("/some/path/any/other/file2.po.yaml");
+
+            gc.Spaces.Should().HaveCount(1);
+
+            var anySpace = gc.Spaces[0];
+            anySpace.Name.Should().Be("any");
+            anySpace.Namespace.Should().Be("A.B.any");
+            anySpace.Spaces.Should().HaveCount(1);
+
+            var otherSpace = anySpace.Spaces[0];
+            otherSpace.Name.Should().Be("other");
+            otherSpace.Namespace.Should().Be("A.B.any.other");
         }
 
         [Test]
@@ -20,6 +32,11 @@ namespace Yapoml.Test.Generation
             var gc = new GlobalGenerationContext("/some/path", "A.B");
 
             gc.AddFile("/some/path/file.po.yaml");
+
+            gc.RootNamespace.Should().Be("A.B");
+            gc.RootDirectoryPath.Should().Be("\\some\\path");
+
+            gc.Spaces.Should().HaveCount(0);
         }
     }
 }

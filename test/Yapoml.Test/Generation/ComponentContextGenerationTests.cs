a01c69e65f62874a1e952b9dad8fac764ea2778a
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2021-12-13 18:43:44 +0300

    Update ComponentContextGenerationTests.cs

--- test/Yapoml.Test/Generation/ComponentContextGenerationTests.cs
+++ test/Yapoml.Test/Generation/ComponentContextGenerationTests.cs
@@ -14,19 +14,48 @@ namespace Yapoml.Test.Generation
         [Test]
         public void Parse_Component()
         {
-            File.WriteAllText("my_page.pc.yaml", @"
+            File.WriteAllText("my_component.pc.yaml", @"
 name: c1
+by: qwe
+
+ya:
+  c2:
+    by: asd
+"
+                );
+
+            var gc = new GlobalGenerationContext(Environment.CurrentDirectory, "A.B", _parser);
+
+            gc.AddFile($"{Environment.CurrentDirectory}\\my_component.pc.yaml");
+
+            gc.Spaces.Should().BeEmpty();
+
+            gc.Components.Should().HaveCount(1);
+
+            var component = gc.Components[0];
+            component.Name.Should().Be("c1");
+
+            component.ComponentGenerationContextes[0].Name.Should().Be("c2");
+        }
+
+        [Test]
+        public void Component_Name_Should_Be_Optional()
+        {
+            File.WriteAllText("my_component.pc.yaml", @"
 by: qwe
 "
                 );
 
             var gc = new GlobalGenerationContext(Environment.CurrentDirectory, "A.B", _parser);
 
-            gc.AddFile($"{Environment.CurrentDirectory}\\my_page.pc.yaml");
+            gc.AddFile($"{Environment.CurrentDirectory}\\my_component.pc.yaml");
 
             gc.Spaces.Should().BeEmpty();
 
             gc.Components.Should().HaveCount(1);
+
+            var component = gc.Components[0];
+            component.Name.Should().Be("my_component");
         }
     }
 }

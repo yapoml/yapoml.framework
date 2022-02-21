07051a955e5a39497830f9257b86b9b20aa6c095
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2022-02-21 20:11:37 +0300

    Clean up tests

--- sample/Yapoml.Playwright.Sample/NuGetSearchTest.cs
+++ sample/Yapoml.Playwright.Sample/NuGetSearchTest.cs
@@ -8,25 +8,35 @@ namespace Yapoml.Selenium.Sample
     [TestFixture]
     public class NuGetSearchTest
     {
-        [TearDown]
-        public void TearDown()
-        {
+        private IPage _page;
+        private IBrowser _browser;
+        private IPlaywright _playwright;
 
+        [SetUp]
+        public async Task SetUp()
+        {
+            _playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+            _browser = await _playwright.Firefox.LaunchAsync(new() { Headless = false});
+            _page = await _browser.NewPageAsync();
+
+            await _page.GotoAsync("https://nuget.org");
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await _page.CloseAsync();
+            await _browser.CloseAsync();
+            _playwright.Dispose();
         }
 
         [Test]
         public async Task Search()
         {
-            using var playwright = await Microsoft.Playwright.Playwright.CreateAsync();
-            await using var browser = await playwright.Firefox.LaunchAsync();
-            var page = await browser.NewPageAsync();
+            await (await _page.QuerySelectorAsync("#search")).TypeAsync("yaml");
+            await _page.ClickAsync(".btn-search");
 
-            await page.GotoAsync("https://nuget.org");
-
-            await (await page.QuerySelectorAsync("#search")).TypeAsync("yaml");
-            await page.ClickAsync(".btn-search");
-
-            foreach (var package in await page.QuerySelectorAllAsync(".package"))
+            foreach (var package in await _page.QuerySelectorAllAsync(".package"))
             {
                 var title = await (await package.QuerySelectorAsync("xpath=.//a")).TextContentAsync();
                 Assert.That(title, Is.Not.Empty);
@@ -37,18 +47,11 @@ namespace Yapoml.Selenium.Sample
         }
 
         [Test]
-        public async Task SearchWithYapoml()
+        public void SearchWithYapoml()
         {
-            using var playwright = await Microsoft.Playwright.Playwright.CreateAsync();
-            await using var browser = await playwright.Firefox.LaunchAsync();
-            var page = await browser.NewPageAsync();
+            var ya = _page.Ya().Pages.NuGet;
 
-            await page.GotoAsync("https://nuget.org");
-
-            var ya = page.Ya().Pages.NuGet;
-
-            await ya.HomePage.SearchInput.TypeAsync("yaml");
-            await ya.HomePage.SearchButton.ClickAsync();
+            ya.HomePage.Search("yaml");
 
             foreach (var package in ya.SearchResultsPage.Packages)
             {

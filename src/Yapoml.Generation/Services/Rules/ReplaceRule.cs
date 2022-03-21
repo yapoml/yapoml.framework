6a5ecd76955b4b195436b616f22d0f3a3985d13d
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2022-03-21 21:50:35 +0300

    Improved pluralization service

---
+++ src/Yapoml.Generation/Services/Rules/ReplaceRule.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Yapoml.Generation.Services.Rules
+{
+    public class ReplaceRule
+    {
+        public Regex Condition { get; set; }
+        public string ReplaceWith { get; set; }
+    }
+}
---
+++ test/Yapoml.Test/Services/PluralizationServiceFixture.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Yapoml.Generation.Services;
+
+namespace Yapoml.Test.Services
+{
+    class PluralizationServiceFixture
+    {
+        [Test]
+        [TestCase("Status", false)]
+        [TestCase("Statuses", true)]
+        public void Should_Verify_IsPlural(string word, bool expectedIsPlural)
+        {
+            var service = new PluralizationService();
+
+            service.IsPlural(word).Should().Be(expectedIsPlural);
+        }
+    }
+}
